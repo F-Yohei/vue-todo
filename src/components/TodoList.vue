689ecf92409ac005e40bd7061d75a2346c975054
Yohei Fukudome <pt83s0502@gmail.com>
2021-01-18 10:13:32 +0900

    ダブルクォーテーションからシングルクォーテーションに書換え。

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -48,49 +48,57 @@
 export default {
   data() {
     return {
-      newTask: "",
+      newTask: '',
       todos: [],
       displayState: ''
     };
   },
   computed: {
     viewTodos() {
-      return this.displayState ? this.todos.filter((todo) => todo.state === this.displayState) : this.todos;
+      return this.displayState
+        ? this.todos.filter(todo => todo.state === this.displayState)
+        : this.todos;
     }
   },
   methods: {
+    //タスクを追加する関数
     addNewTask(e) {
       e.preventDefault();
       if (this.newTask.match(/\S/g)) {
         this.todos.push({
           id: this.todos.length,
           task: this.newTask,
-          state: "作業中"
+          state: '作業中'
         });
       }
-      this.newTask = "";
+      this.newTask = '';
     },
+    //タスクを削除する関数
     deleteTask(id) {
       if (id > -1) {
         this.todos.splice(id, 1);
-        this.todos.forEach((todo,index) => {
+        this.todos.forEach((todo, index) => {
           todo.id = index;
-        })
+        });
       }
     },
+    //各オブジェクトが保持しているstateの状態を変更する関数
     changeState(id) {
-      if (this.todos[id].state === "作業中") {
-        this.todos[id].state = "完了";
-      } else if (this.todos[id].state === "完了") {
-        this.todos[id].state = "作業中";
+      if (this.todos[id].state === '作業中') {
+        this.todos[id].state = '完了';
+      } else if (this.todos[id].state === '完了') {
+        this.todos[id].state = '作業中';
       }
     },
+    //displayStateの状態を作業中に変更する関数
     workingTodo() {
-        this.displayState = '作業中';
+      this.displayState = '作業中';
     },
+    //displayStateの状態を完了に変更する関数
     doneTodo() {
       this.displayState = '完了';
     },
+    //displayStateの状態を空にする関数
     allTodo() {
       this.displayState = '';
     }
